027bd898597fda281af9e8d50c7a2da8789a09e2
jayb <jaydipshura@gmail.com>
2020-05-13 18:36:17 +0530

    item-click-render

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,12 +1,69 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import "../styles/about.css";
 import ItemBox from "./common/item-box";
 import eduImg from "../img/education.png";
 import workImg from "../img/work.png";
 import personalImg from "../img/personal.png"
 
-const About = props => {
+class About extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDetail: ""
+    };
+    this.onItemClick = this.onItemClick.bind(this);
+  }
 
+  onItemClick(value) {
+    this.setState({
+     selectedDetail: value
+   });
+  }
+
+  renderBackButton() {
+    return (
+      <button className="btn btn-success back-btn" onClick={() => this.onItemClick("")}>Back</button>
+    );
+  }
+
+  renderEducatiion () {
+    return (
+      <div>
+        {this.renderBackButton()}
+        <h1>Education</h1>
+      </div>
+    );
+  }
+
+  renderExperience () {
+    return (
+      <div>
+        {this.renderBackButton()}
+        <h1>Experience</h1>
+      </div>
+    );
+  }
+
+  renderPersonal () {
+    return (
+      <div>
+        {this.renderBackButton()}
+        <h1>Personal</h1>
+      </div>
+    );
+  }
+
+  renderPhotograpy () {
+    return (
+      <div>
+        {this.renderBackButton()}
+        <h1>Images/Video</h1>
+        Coming Soon...
+      </div>
+    );
+  }
+
+  renderAboutTitle() {
     return (
       <div className="container-fluid">
         <div className="row about-row-container">
@@ -14,11 +71,13 @@ const About = props => {
             alt="Education"
             icon={eduImg}
             name="Education"
+            onClick={this.onItemClick}
             />
           <ItemBox
             alt="Work Experience"
             icon={workImg}
             name="Work Experience"
+            onClick={this.onItemClick}
             />
         </div>
         <div className="row about-row-container">
@@ -26,15 +85,33 @@ const About = props => {
             alt="Personal"
             icon={personalImg}
             name="Personal"
+            onClick={this.onItemClick}
             />
           <ItemBox
             alt=""
             icon=""
-            name="Other Stuff"
+            name="Videos/Images"
+            onClick={this.onItemClick}
             />
         </div>
       </div>
     );
-};
+  }
 
+  render() {
+    const { selectedDetail } = this.state;
+    console.log(selectedDetail);
+    if (selectedDetail === "Education") {
+      return this.renderEducatiion();
+    } else if (selectedDetail === "Work Experience") {
+      return this.renderExperience();
+    } else if (selectedDetail === "Personal") {
+      return this.renderPersonal();
+    } else if (selectedDetail === "Videos/Images") {
+      return this.renderPhotograpy();
+    }
+    return this.renderAboutTitle();
+  }
+}
+About.displayName = "About";
 export default About;
--- src/components/common/item-box.jsx
+++ src/components/common/item-box.jsx
@@ -4,32 +4,35 @@ import PropTypes from 'prop-types';
 import '../../styles/item-box.css';
 
 const ItemBox = props => {
-    const { alt, icon, name } = props;
+    const { alt, icon, name, onClick } = props;
     return (
-      <div className="col-sm-5 about-items shadow p-3">
+      <button className="col-sm-5 about-items shadow p-3" onClick={() => {onClick(name)}}>
         <img
         src={icon}
         alt={alt}
         />
         <h3>{name}</h3>
-      </div>
+
+      </button>
     );
 };
 
 ItemBox.displayName = "ItemBox";
 
 ItemBox.propTypes = {
-  description: PropTypes.string,
-  icon: PropTypes.object,
   alt: PropTypes.string,
-  name: PropTypes.string
+  description: PropTypes.string,
+  icon: PropTypes.string,
+  name: PropTypes.string,
+  onClick: PropTypes.func
 };
 
 ItemBox.defaultProps = {
+  alt: "",
   description: "",
   icon: "",
-  alt: "",
-  name: ""
+  name: "",
+  onClick() {}
 };
 
 export default ItemBox;
