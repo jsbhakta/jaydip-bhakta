553d0b091aad535f997ba483d54f80d56f985f3d
jayb <jaydipshura@gmail.com>
2020-05-17 18:45:15 +0530

    work-experience-added

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -78,6 +78,133 @@ class About extends PureComponent {
       <div>
         {this.renderBackButton()}
         <h1>Experience</h1>
+          <table className="table table-bordered table-hover">
+            <tbody>
+              <tr>
+                <td><h4>SUMMARY</h4></td>
+                <td>
+                  <ul>
+                    <li>
+                      Working experience of developing optimized website architecture
+                    </li>
+                    <li>
+                      Extensive knowledge of developing optimized website using React.js, JavaScript, HTML5 and CSS
+                    </li>
+                    <li>
+                      Strong website testing and data/network security management skills
+                    </li>
+                    <li>
+                      Excellent communication and consultation skills
+                    </li>
+                    <li>
+                      Demonstrated ability to work independently as well as in a team environment
+                    </li>
+                    <li>
+                      Quick learner with strong desire and aptitude to learn new tools and techniques
+                    </li>
+                  </ul>
+                </td>
+              </tr>
+              <tr>
+                <td><h4>TECHNICAL SKILLS:</h4></td>
+                <td>
+                  <ul>
+                    <li className="row text-right">
+                      <strong className="col-sm-3">Web Development:</strong>React.js, JavaScript, Node.js, Bootstrap, HTML5 and CSS
+                    </li>
+                    <li className="row text-right">
+                      <strong className="col-sm-3">Programing Languages: </strong>Python, Scala, C++, Java, XML and R
+                    </li>
+                    <li className="row text-right">
+                      <strong className="col-sm-3">Scripting Languages: </strong>Shell, Perl
+                    </li>
+                    <li className="row text-right">
+                      <strong className="col-sm-3">Database: </strong>MySQL/SQL, MongoDB,
+                    </li>
+                    <li className="row text-right">
+                      <strong className="col-sm-3">Tools: </strong>Xcode, Sublime, Atom, Visual Studio,
+                    </li>
+                    <li className="row text-right">
+                      <strong className="col-sm-3">Application: </strong>TIBCO, git, Git Hub Adobe Photoshop; Flash,
+                    </li>
+                    <li className="row text-right">
+                      <strong className="col-sm-3">Operating System: </strong>Windows, macOS, Android, Linux
+                    </li>
+                  </ul>
+                </td>
+              </tr>
+              <tr>
+                <td><h4>VOLUNTEER WORK:</h4></td>
+                <td>
+                  Code for Fun (School), Fremont, CA Oct 2015 – May 2016
+                  <p>Volunteer Teaching Assistant:</p>
+                  <ul>
+                    <li>
+                      Provided technical support to setup coding environment on computers
+                    </li>
+                    <li>
+                      Communicated and coordinated with students and lead instructor
+                    </li>
+                    <li>
+                      Organized and supported promotional events
+                    </li>
+                  </ul>
+                </td>
+              </tr>
+              <tr>
+                <td><h4>EXPERIENCE:</h4></td>
+                <td>
+                    Northwestern Polytechnic University - Fremont, CA Sep 2016 – Sept 2017
+                    <p>Career Services Assistant | Graduate Teaching Assistant</p>
+                    <ul>
+                      <li>
+                        Provided technical support using Desktop applications including MS Word, MS Excel, Adobe Reader, and Adobe Photoshop
+                      </li>
+                      <li>
+                        Organizing and managing career department events
+                      </li>
+                      <li>
+                        Communicating and coordinating with students and companies for career fair event on regular basis
+                      </li>
+                    </ul>
+                    Northwestern Polytechnic University - Fremont, CA Jan 2016 – Aug 2016
+                    <p>Lab Instructor</p>
+                    <ul>
+                      <li>
+                        Taught more than 50 graduate students – C++ Object Oriented Programming
+                      </li>
+                      <li>
+                        Designed grading rubrics and course activities including Lab Assignments, Quizzes and Exams
+                      </li>
+                      <li>
+                        Coordinated with the class instructor to get suggestion and recommendation on lab material
+                      </li>
+                      <li>
+                        Taught to write optimize code and test security and quality control of the software
+                      </li>
+                    </ul>
+                </td>
+              </tr>
+              <tr>
+                <td><h4>SCHOOL PROJECT:</h4></td>
+                <td>
+                  Stock Recommendation & Predicting System - Website, <i>Jan 2018 – Apr 2018</i>
+                <p>Role: Team Leader / Full Stack Developer</p>
+                <ul>
+                  <li>
+                    Developed 5 different modules using python 3.6 for stock recommendation
+                  </li>
+                  <li>
+                    Created algorithm to recommend stocks based on stock performance and user needs
+                  </li>
+                  <li>
+                    Design the dynamic web pages using HTML5 & CSS3 and javaScript
+                  </li>
+                </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
       </div>
     );
   }
@@ -87,6 +214,7 @@ class About extends PureComponent {
       <div>
         {this.renderBackButton()}
         <h1>Personal</h1>
+        <i>Coming Soon...</i>
       </div>
     );
   }
@@ -96,7 +224,7 @@ class About extends PureComponent {
       <div>
         {this.renderBackButton()}
         <h1>Images/Video</h1>
-        Coming Soon...
+        <i>Coming Soon...</i>
       </div>
     );
   }
